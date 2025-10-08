ac3ae059a3e0d1a71588b20ce61d0470f1f3ddf9
LucasLealAI <lucas2006leal@gmail.com>
2025-10-08 05:24:19 -0300

    Atualizado Canvas

--- scriptLab5.js
+++ scriptLab5.js
@@ -1,2 +1,18 @@
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'blue';
+ctx.strokeStyle = 'white';
+ctx.fillRect(10,10,50,50);
+ctx.strokeRect(10,10,50,50);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'red';
+ctx.strokeStyle = 'white';
+ctx.fillRect(10,10,10,10);
+ctx.strokeRect(10,10,10,10);
+ctx.closePath();
