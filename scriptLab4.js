34be471fdc1a3a12010fbafdcf310d598482de6f
LucasLealAI <lucas2006leal@gmail.com>
2025-10-09 16:53:01 -0300

    Feito

--- scriptLab4.js
+++ scriptLab4.js
@@ -1,26 +1,29 @@
 let numeroSecreto = Math.floor(Math.random() * 100);
 
+console.log(numeroSecreto)
+
 let menores = [];
 let maiores = [];
 
 function imprimeGuess() {
-  let numero = document.getElementById("numero");
-  let entrada = document.getElementById("entrada");
+  let numero = parseInt(document.getElementById("numero").value);
+  
+  console.log(numero)
 
   let mensagem = document.getElementById("mensagem");
 
-  if (entrada === numeroSecreto) {
+  if (numero === numeroSecreto) {
     mensagem.textContent = "número igual"
     mensagem.style.setProperty("background-color", "green")
-  } else if (entrada < numeroSecreto) {
+  } else if (numero < numeroSecreto) {
     mensagem.textContent = "número maior" 
     mensagem.style.setProperty("background-color", "red")
     menores.push(numero)
-    document.getElementById("menores").textContent = menores
-  } else if (entrada > numeroSecreto) {
+    document.getElementById("menores").innerHTML = menores
+  } else if (numero > numeroSecreto) {
     mensagem.textContent = "número menor";
     mensagem.style.setProperty("background-color", "red");
     maiores.push(numero);
-    document.getElementById("maiores").textContent = maiores
+    document.getElementById("maiores").innerHTML = maiores
   }
 }
