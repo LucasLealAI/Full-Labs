01880bd0ec3c0cb9f8633cec641a99da22cdd8c7
LucasLealAI <lucas2006leal@gmail.com>
2025-10-09 02:16:24 -0300

    Feito mais do Lab 5

--- scriptLab5.js
+++ scriptLab5.js
@@ -4,15 +4,65 @@ let ctx = canvas.getContext('2d');
 ctx.beginPath();
 ctx.lineWidth = 2;
 ctx.fillStyle = 'blue';
-ctx.strokeStyle = 'white';
-ctx.fillRect(10,10,50,50);
-ctx.strokeRect(10,10,50,50);
+ctx.fillRect(0,0,60,60);
 ctx.closePath();
 
 ctx.beginPath();
 ctx.lineWidth = 2;
 ctx.fillStyle = 'red';
-ctx.strokeStyle = 'white';
-ctx.fillRect(10,10,10,10);
-ctx.strokeRect(10,10,10,10);
+ctx.fillRect(340,0,60,60);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'cyan';
+ctx.fillRect(360,180,40,40);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'cyan';
+ctx.fillRect(0,165,40,40);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'cyan';
+ctx.fillRect(0,200,40,40);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'yellow';
+ctx.fillRect(0,360,40,40);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'yellow';
+ctx.fillRect(0,320,40,40);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'yellow';
+ctx.fillRect(40,360,40,40);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'black';
+ctx.fillRect(360,360,40,40);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'black';
+ctx.fillRect(360,320,40,40);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'black';
+ctx.fillRect(320,360,40,40);
 ctx.closePath();
