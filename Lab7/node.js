dd10205eb865177c6e969ca1caa532bbb9967ce1
LucasLealAI <lucas2006leal@gmail.com>
2025-10-23 16:15:56 -0300

    Adicionado Lab 7

---
+++ Lab7/node.js
@@ -0,0 +1,10 @@
+require("colors");
+
+var http = require('http');
+var express = require("express");
+
+var app = express();
+app.use(express.static('./public'));
+
+var server = http.createServer(app);
+server.listen(80);
