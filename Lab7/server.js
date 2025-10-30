54ab57d3c1c4494456cb066ddcc2daede02d2510
LucasLealAI <lucas2006leal@gmail.com>
2025-10-30 16:56:49 -0300

    Um pouco do Lab 7 pra te ajudar

--- Lab7/server.js
+++ Lab7/server.js
@@ -12,5 +12,21 @@ server.listen(80);
 console.log("servidor rodando...".rainbow)
 
 app.get('/', function(requisicao, resposta){
-    resposta.redirect('Lab1/Lab1.html');
+    resposta.redirect('Lab1.html');
+})
+
+// é tudo ez, só falta tu saber a diferença de get e post e dps só copiar oq
+// voce fez na aula 9 que tá lá no github tlg?
+
+app.get('/cadastrar',function(requisicao, resposta){
+    console.log('Requisicao recebida por get!');
+
+    let nome = requisicao.query.nome
+    let email = requisicao.query.email;
+    let telefone = requisicao.query.telefone
+    let nascimento = requisicao.query.nascimento;
+
+    console.log(nome, email, telefone, nascimento)
+
+    resposta.render("resposta.ejs", {metodo: "GET", nome, email, telefone, nascimento});
 })
