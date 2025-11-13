01de6e54765dbd3534fc7f3505168910b4a931eb
LucasLealAI <lucas2006leal@gmail.com>
2025-11-13 05:02:32 -0300

    Terminado Lab 7

--- Lab7/server.js
+++ Lab7/server.js
@@ -2,31 +2,55 @@ require("colors");
 
 var http = require('http');
 var express = require('express');
+var bodyParser = require("body-parser")
 
 var app = express();
 app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({extended: false }))
+app.use(bodyParser.json())
+app.set('view engine', 'ejs')
+app.set('views', './views');
 
 var server = http.createServer(app);
 server.listen(80);
 
 console.log("servidor rodando...".rainbow)
 
-app.get('/', function(requisicao, resposta){
-    resposta.redirect('Lab1.html');
-})
 
 // é tudo ez, só falta tu saber a diferença de get e post e dps só copiar oq
 // voce fez na aula 9 que tá lá no github tlg?
 
-app.get('/cadastrar',function(requisicao, resposta){
-    console.log('Requisicao recebida por get!');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "Lab2project.html");
+});
 
-    let nome = requisicao.query.nome
-    let email = requisicao.query.email;
-    let telefone = requisicao.query.telefone
-    let nascimento = requisicao.query.nascimento;
+app.get("/cadastra", (req, res) => {
+  res.sendFile(__dirname + "Lab7cadastro.html");
+});
 
-    console.log(nome, email, telefone, nascimento)
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "Lab7login.html");
+});
 
-    resposta.render("resposta.ejs", {metodo: "GET", nome, email, telefone, nascimento});
+app.post("/cadastrar", function(req, res) {
+    const { nome, senha } = req.body;
+    const existente = usuarios.find(u => u.nome === nome);
+    
+    if (existente) {
+        res.render("resposta", {mensagem: "Usuário já cadastrado!"})
+    } else {
+        usuarios.push({ nome, senha })
+        res.render("resposta", {mensagem: "Cadastro realizado com sucesso!"})
+    }
+})
+
+app.post("/logar", (req, res) => {
+    const { nome, senha } = req.body;
+    const usuario = usuarios.find(u => u.nome === nome && u.senha === senha);
+
+    if (usuario) {
+        res.render("resposta", {mensagem: "Bem-vindo, ${nome}"})
+    } else {
+        res.render("resposta", {mensagem: "Falha no usuário, tente novamente."})
+    }
 })
